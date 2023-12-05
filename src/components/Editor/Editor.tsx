d86527a778fec6bb1b0bb70f806263a989c47463
Yannick Perret <contact@yannickperret.com>
2023-12-05 10:27:21 +0100

    show parse markedown in vue

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
-import data from "../../data/workspace";
-import EditorToolsBar from './EditorToolsBar';
+import React, { useEffect, useState } from 'react';
+import { marked } from 'marked';
 import editorStyle from './Editor.module.scss'
+import data from "../../data/workspace";
+
+
 
 export default function Editor() {
-    const [editorState, setEditorState] = useState(data.collections[0].resources[0].content)
-  return (
-    <div className={editorStyle.editor}>
-        <textarea value={editorState} onChange={(e) => setEditorState(e.target.value)} />
-        <EditorToolsBar />
-    </div>
-  )
+    const [editorState, setEditorState] = useState(data.collections[0].resources[0].content);
+    const [html, setHtml] = useState('');
+
+
+    useEffect(() => {
+        async function parseMarkdown() {
+            const content = await editorState;
+            const rawHtml = marked.parse(content);
+            setHtml(rawHtml);
+        }
+        parseMarkdown();
+    }, [editorState]);
+
+    return (
+        <div className={editorStyle.editor} dangerouslySetInnerHTML={{ __html: html }} />
+    );
 }
