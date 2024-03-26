bca07b53c4567c1bec47ac02f95faaa181977e29
Yannick Perret <contact@yannickperret.com>
2024-03-26 22:13:32 +0100

    change import

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,7 @@
-import { useParams } from "react-router-dom";
-import Editor from "../../components/editor/Editor.jsx";
-import { EditorOptionsProvider } from "../../providers/EditorOptionsProvider.jsx";
+import Editor from "@/components/Editor/Editor.jsx";
+import { EditorOptionsProvider } from "@/providers/EditorOptionsProvider";
 
 export default function Home() {
-  const { workspaceId } = useParams();
   return (
     <EditorOptionsProvider>
       <Editor />
