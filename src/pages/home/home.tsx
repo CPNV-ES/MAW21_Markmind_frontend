e4d9727f5120f317c6088adb34d548ccf9dd56d7
menoudyann <Yann.MENOUD@cpnv.ch>
2024-03-26 19:24:26 +0100

    clean the project of unused files after the merge

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,6 @@
-import { EditorOptionsProvider } from "../../providers/EditorOptionsProvider.jsx";
-import Editor from "../../components/editor/Editor";
-import React from "react";
 import { useParams } from "react-router-dom";
+import Editor from "../../components/editor/Editor.jsx";
+import { EditorOptionsProvider } from "../../providers/EditorOptionsProvider.jsx";
 
 export default function Home() {
   const { workspaceId } = useParams();
@@ -9,5 +8,5 @@ export default function Home() {
     <EditorOptionsProvider>
       <Editor />
     </EditorOptionsProvider>
-  )
+  );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,6 @@
 import BaseLayout from "@/layouts/BaseLayout/BaseLayout";
 import GuestLayout from "@/layouts/GuestLayout/GuestLayout.tsx";
-import Home from "@/pages/Home/Home";
+import Home from "@/pages/Home/home";
 import Login from "@/pages/Login/Login.tsx";
 import { createBrowserRouter } from "react-router-dom";
 
